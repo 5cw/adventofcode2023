2ba08ebdcf1ddcbb101ed0a0d3626787b02ed1c5
Lexi <kreinsbo@uvm.edu>
2023-12-11 14:34:48 -0500

    day11 all caught up!

---
+++ day11.py
@@ -0,0 +1,18 @@
+import itertools
+
+inp = open('day11input.txt').readlines()
+rows = [i for i in range(len(inp)) if '#' not in inp[i]]
+cols = [i for i in range(len(inp[0])) if '#' not in [line[i] for line in inp]]
+for factor in [2, 1000000]:
+    galaxies = []
+    for i in range(len(inp)):
+        for j in range(len(inp[0])):
+            if inp[i][j] == '#':
+                y = i + len([row for row in rows if row < i])*(factor - 1)
+                x = j + len([col for col in cols if col < j])*(factor - 1)
+                galaxies += [(x, y)]
+
+    total = 0
+    for ((x1, y1), (x2, y2)) in itertools.combinations(galaxies, 2):
+        total += abs(x2 - x1) + abs(y2 - y1)
+    print(total)
